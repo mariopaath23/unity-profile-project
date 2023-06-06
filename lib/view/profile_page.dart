c11ba79d2b568d120439a5b06794281f867828ea
Mario Paath <mariopaath23@gmail.com>
2023-06-06 20:18:48 +0800

    Update repository to current progress

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,10 +1,39 @@
+import 'dart:convert';
+import 'dart:html';
 import 'package:flutter/material.dart';
-
+import '../models/user_model.dart';
 import '../theme.dart';
+import 'package:http/http.dart' as http;
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
+  
+  @override
+  State<ProfilePage> createState() => ProfilePageState();
+}
 
+class _ProfilePageState extends State<ProfilePage>{
+  Future<Data> getUserData() async{
+    final response = await http.get(
+      Uri.parse('https://reqres.in/api/users/2')
+      );
+
+      if (response.statusCode == 200){
+        final body = jsonDecode(response.body);
+        Data userData = UserModel.fromJson(body).data;
+        return userData;
+      }
+      else{
+        throw 'Error!';
+      }
+  }
+  late Future<Data> user;
+
+  @override
+  void initState(){
+    super.initState();
+    user = getUserData();
+  }
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -20,19 +49,29 @@ class ProfilePage extends StatelessWidget {
                 fit: BoxFit.cover),
           ),
         ),
-        Center(
+        FutureBuilder(future: user, builder: (ctx, snapshot){
+          var connectionState = snapshot.connectionState;
+          if(connectionState == ConnectionState.none){
+            return const Text('Sedang Offline');
+          }
+          else if(connectionState == ConnectionState.waiting){
+            return const CircularProgressIndicator();
+          }
+          else if(connectionState == ConnectionState.done){
+            if(snapshot.hasData){
+              return Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const CircleAvatar(
                 radius: 55,
-                backgroundImage: AssetImage('assets/Me.jpeg'),
+                backgroundImage: NetworkImage(snapshot.data!.avatar),
               ),
               const SizedBox(
                 height: 5,
               ),
               Text(
-                'Mario Paath',
+                '${snapshot.data!.firstName} ${snapshot.data!.lastName}',
                 style: titleTextStyle.copyWith(
                     letterSpacing: 2,
                     color: Colors.white,
@@ -43,17 +82,21 @@ class ProfilePage extends StatelessWidget {
                 height: 10,
               ),
               Text(
-                'Not Senior Flutter Developer',
+                'Not So Senior Flutter Developer',
                 style: titleTextStyle.copyWith(
                     color: Colors.white, fontWeight: FontWeight.bold),
               ),
               const SizedBox(
                 height: 5,
               ),
-              const Text('apa itu flutter?')
+              Text(snapshot.detail.email)
             ],
           ),
         )
+            }
+          }
+          return const Center(child: Text('No Data'));
+        })
       ],
     );
   }
